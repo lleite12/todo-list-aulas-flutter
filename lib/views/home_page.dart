2167f0aac804a1c81945e896645cf49fd08eee1f
Lab2d <lab2d@admin.local>
2019-10-28 21:31:36 -0300

    commit 4

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -20,11 +20,11 @@ class _HomePageState extends State<HomePage> {
   double _lenghtBD;
 
   List<Color> _colorsList = [
-    Colors.blue.shade100,
-    Colors.green.shade100,
+    Colors.blue.shade50,
     Colors.yellow.shade100,
-    Colors.orange.shade100,
-    Colors.red.shade100
+    Colors.yellow.shade300,
+    Colors.orange.shade300,
+    Colors.red.shade300
   ];
 
    @override
